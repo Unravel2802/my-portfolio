65c2400dce5b7b47a8c0e800a4e836af75b47115
Unravel2802 <hoanglyquoc@gmail.com>
2025-03-11 09:09:27 -0400

    basic component's code

---
+++ src/components/Contact.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function Contact() {
+    return (
+        <section id="contact" className="contact">
+        <h2>Contact</h2>
+        <ul>
+            <li>Email: hoangqly2004@gmail.com</li>
+            <li>Phone: 2073071607</li>
+            <li>LinkedIn: </li>
+            <li>GitHub: </li>
+        </ul>
+        </section>
+    );
+}
+
+export default Contact;
