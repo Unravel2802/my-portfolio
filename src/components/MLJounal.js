6890a0f40e9062d2219a043d541a92a0c19ea096
Unravel2802 <hoanglyquoc@gmail.com>
2025-03-11 09:14:49 -0400

    adding a page for ml journal

---
+++ src/components/MLJounal.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function MLJournal() {
+    return (
+        <section id="mljournal" className="mljournal">
+            <h2> Machine Learning research papers jounrnal</h2>
+            <p> This is my journal where I keep track of the research papers that I have read</p>
+        </section>
+    );
+}
+
+export default MLJournal;
