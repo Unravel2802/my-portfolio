58faf9489131c72a93037d32aa879ed8f6de408b
Unravel2802 <hoanglyquoc@gmail.com>
2025-03-12 14:21:19 -0400

    adding description phrase to home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,37 @@
+import { motion } from 'framer-motion';
 import '../styles/global.css';
 
 export default function Home() {
   return (
-    <div>
-        <h1>Hoang Quoc Ly</h1>
+    <div className="home-container">
+      <motion.h1 
+        className="name-title"
+        initial={{ opacity: 0, y: -20 }} 
+        animate={{ opacity: 1, y: 0 }} 
+        transition={{ duration: 0.8 }}
+      >
+        Hoang Q. Ly
+      </motion.h1>
+
+      <div className="description-container">
+        <motion.h1 
+          className="description"
+          initial={{ opacity: 0, x: -50 }} 
+          animate={{ opacity: 1, x: 0 }} 
+          transition={{ duration: 1 }}
+        >
+          Polymath
+        </motion.h1>
+        
+        <motion.h1 
+          className="description"
+          initial={{ opacity: 0, x: 50 }} 
+          animate={{ opacity: 1, x: 0 }} 
+          transition={{ duration: 1, delay: 0.3 }}
+        >
+          Developer
+        </motion.h1>
+      </div>
     </div>
   );
 }
