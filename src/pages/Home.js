f4ead4be873ebc7fa5080b71f07590b747d27528
Unravel2802 <hoanglyquoc@gmail.com>
2025-03-13 14:47:02 -0400

    adding why polymath section to landing for scroll down

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,6 +2,13 @@ import { motion } from 'framer-motion';
 import '../styles/global.css';
 
 export default function Home() {
+  const scrollToSection = () => {
+    const section = document.getElementById("why-polymath");
+    if (section) {
+      section.scrollIntoView({ behavior: "smooth" });
+    }
+  };
+
   return (
     <div className="home-container">
       <motion.h1 
@@ -14,6 +21,17 @@ export default function Home() {
       </motion.h1>
 
       <div className="description-container">
+
+        <motion.h1 
+          className="description"
+          initial={{ opacity: 0, x: 50 }} 
+          animate={{ opacity: 1, x: 0 }} 
+          transition={{ duration: 1 }}
+        >
+          The
+        </motion.h1>
+        
+
         <motion.h1 
           className="description"
           initial={{ opacity: 0, x: -50 }} 
@@ -32,6 +50,24 @@ export default function Home() {
           Developer
         </motion.h1>
       </div>
+      <motion.h2
+          className="why-polymath"
+          initial={{ opacity: 0, y: 20 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 1, delay: 0.5 }}
+      >
+        Why polymath? 
+        
+      </motion.h2>
+      <motion.h2
+          className="arrow"
+          initial={{ opacity: 0, y: 20 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 1, delay: 0.5 }}
+      >
+      ↓        
+      </motion.h2>
+      
     </div>
   );
 }
